6dd17ae5e8016a0f6c8d106a344b5956b293c9b5
Waynexia888 <qiwuwaynexia@gmail.com>
2020-02-21 17:12:19 -0800

    basic CRUD Features

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.urls import reverse
+
+from imagekit.models import ProcessedImageField
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- Insta/views.py
+++ Insta/views.py
@@ -2,7 +2,35 @@
 
 # # Create your views here.
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+
