2ea18f3a1768dd864fb693e3ad8297992fc7b170
Waynexia888 <qiwuwaynexia@gmail.com>
2020-02-23 17:15:46 -0800

    added user login mixin and user signup

--- Insta/views.py
+++ Insta/views.py
@@ -5,8 +5,11 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 from Insta.models import Post
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
@@ -19,10 +22,12 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -33,4 +38,9 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
 
