c872db1da2165b37aa2980ffddae5ab9621b2b87
Waynexia888 <qiwuwaynexia@gmail.com>
2020-02-26 17:54:44 -0800

    updated index.html, post detail

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from Insta.models import Post, InstaUser
+from Insta.models import Post, InstaUser, Like
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(InstaUser)
+admin.site.register(Like)
--- Insta/models.py
+++ Insta/models.py
@@ -8,22 +8,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Post(models.Model):
-    title = models.TextField(blank=True, null=True)
-    image = ProcessedImageField(
-        upload_to='static/images/posts',
-        format='JPEG',
-        options={'quality':100},
-        blank=True,
-        null=True,
-    )
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[str(self.id)])
-
 class InstaUser(AbstractUser):
     profile_pic = ProcessedImageField(
         upload_to='static/images/profiles',
@@ -34,4 +18,47 @@ class InstaUser(AbstractUser):
     )
 
 
+class Post(models.Model):
+    author = models.ForeignKey(
+       InstaUser,
+       on_delete=models.CASCADE,
+       related_name='my_posts'
+    )
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True,
+    )
+
+    def get_like_count(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+
+ 
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
+        )
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+        )
+
+    class Meta:
+        unique_together = ("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
 
